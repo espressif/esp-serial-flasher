1d3e2f4a4b42f02f94874369a8107e85cfca3ab7
Jaroslav Burian <jaroslav.burian@espressif.com>
2025-01-02 10:38:20 +0100

    ci: Update copyright year of esp_stubs to 2025
    This updates the copyright year of the esp_stubs files to 2025.
    The change is necessary to make check_stub_source_correctness test
    to pass as it checks difference between embedded stub files and the
    original release.

--- private_include/esp_stubs.h
+++ private_include/esp_stubs.h
@@ -1,4 +1,4 @@
-/* Copyright 2020-2024 Espressif Systems (Shanghai) CO LTD
+/* Copyright 2020-2025 Espressif Systems (Shanghai) CO LTD
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
