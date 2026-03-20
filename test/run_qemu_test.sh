9bbc2fadf432855afde0986bc2625f4553b699c2
Jaroslav Burian <jaroslav.burian@espressif.com>
2026-03-20 14:20:32 +0100

    test(qemu): Update QEMU test to use ESP-IDF environment

--- test/run_qemu_test.sh
+++ test/run_qemu_test.sh
@@ -7,12 +7,9 @@ dd if=/dev/urandom bs=1024 count=64 of="$(dirname "$0")/hello-world.bin"
 
 mkdir -p build && cd build
 
-# QEMU_PATH environment variable has to be defined, pointing to qemu-system-xtensa
-# Example: export QEMU_PATH=/home/user/esp/qemu/xtensa-softmmu/qemu-system-xtensa
-if [ -z "${QEMU_PATH}" ]; then
-    echo "QEMU_PATH environment variable needs to be set"
-    exit 1
-fi
+# qemu-system-xtensa is expected to be available on PATH.
+# See https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-guides/tools/qemu.html
+QEMU_PATH="${QEMU_PATH:-qemu-system-xtensa}"
 
 # Generate empty file into which application will be flashed and compared against
 dd if=/dev/zero bs=1024 count=4096 of="empty_file.bin"
